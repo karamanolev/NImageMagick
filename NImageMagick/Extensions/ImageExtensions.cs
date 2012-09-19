eebd9c0dbee690a9155fb7f1a883ebf2cc0e8c26
Ivailo Karamanolev <ivailo@karamanolev.com>
2012-09-19 02:28:15 -0700

    Get the extension methods for enumerables back in.

--- NImageMagick/Extensions/ImageExtensions.cs
+++ NImageMagick/Extensions/ImageExtensions.cs
@@ -6,5 +6,40 @@ namespace NImageMagick.Extensions
 {
     public static class ImageExtensions
     {
+        public static IEnumerable<Image> Crop(this IEnumerable<Image> images, int width, int height, int x, int y)
+        {
+            foreach (Image image in images)
+            {
+                image.Crop(width, height, x, y);
+                yield return image;
+            }
+        }
+
+        public static IEnumerable<Image> Fit(this IEnumerable<Image> images, int width, int height)
+        {
+            foreach (Image image in images)
+            {
+                image.Fit(width, height);
+                yield return image;
+            }
+        }
+
+        public static IEnumerable<Image> Fit(this IEnumerable<Image> images, int size)
+        {
+            foreach (Image image in images)
+            {
+                image.Fit(size);
+                yield return image;
+            }
+        }
+
+        public static IEnumerable<Image> Resize(this IEnumerable<Image> images, int width, int height)
+        {
+            foreach (Image image in images)
+            {
+                image.Resize(width, height, FilterType.LanczosSharpFilter, 1);
+                yield return image;
+            }
+        }
     }
 }
